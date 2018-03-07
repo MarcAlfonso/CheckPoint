14db10008fdcbd7ccc13a63b2d79f5ac2a220f06
ALFONSO_SECANELLA <marc.alfonso@balmes.escolapia.net>
2018-03-07 19:22:10 +0100

    Simple movement done

--- Checkpoint/Assets/Script/CheckPoint.cs
+++ Checkpoint/Assets/Script/CheckPoint.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour {
+    public float moveSpeed = 10f;
+    public float jumpSpeed = 20f;
+    public GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,30 @@ public class CheckPoint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Movement();
 	}
+
+    void Movement()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            player.transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            player.transform.Translate(Vector3.up * Time.deltaTime * jumpSpeed);
+        }
+    }
 }
