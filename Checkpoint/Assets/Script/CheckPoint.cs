6b6c4bcfd2764606a7371d059878f2dfad66d677
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-03-13 21:05:26 +0100

    queda poquito para acabar el checkpoint, script principal avanzado y al collisionar ya cambia a una posicion estandar

--- Checkpoint/Assets/Script/CheckPoint.cs
+++ Checkpoint/Assets/Script/CheckPoint.cs
@@ -2,29 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPoint : MonoBehaviour {
+public class CheckPoint : MonoBehaviour
+{
     public float moveSpeed = 10f;
-    public float jumpSpeed = 20f;
-    public GameObject check_point;
-    public GameObject player;
+    public float jumpForce = 20f;
+    public GameObject CP, player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private bool isActivated = false;
+    private Vector3 checkPos, playerPos;
+    private bool isActive = true;
+    private bool cpActived = false;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        checkPos = CP.transform.position;
+        playerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Movement();
-	}
+        //Debug.Log(playerPos);
+    }
 
     void Movement()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             player.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
-        else if(Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             player.transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
         }
@@ -36,17 +47,34 @@ public class CheckPoint : MonoBehaviour {
         {
             player.transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
         }
-        else if(Input.GetKey(KeyCode.Space))
+        else if (Input.GetKey(KeyCode.Space))
         {
-            player.transform.Translate(Vector3.up * Time.deltaTime * jumpSpeed);
+            player.transform.Translate(Vector3.up * Time.deltaTime * jumpForce);
         }
     }
-    
-    void OnCollisionEnter(Collision col)
+    public IEnumerator Wait()
     {
-        if(col.gameObject.name == "CheckPoint")
+        cpActived = true;
+        player.SetActive(false);
+        yield return new WaitForSeconds(2f);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "checkpoint")
         {
-            Debug.Log("Has chocado con el checkpoint!");
+            StartCoroutine(Wait());
+            player.transform.position = Vector3.zero;
+            player.SetActive(true);
         }
+
+
+    }
+
+
+    void ActivateCheckPoint()
+    {
+
     }
 }
+   
