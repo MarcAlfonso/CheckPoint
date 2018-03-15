e36f6ae0c9b1a3716c390e185d41827602686722
ALFONSO_SECANELLA <marc.alfonso@balmes.escolapia.net>
2018-03-15 17:54:05 +0100

    checkpoint done

--- Checkpoint/Assets/Script/CheckPoint.cs
+++ Checkpoint/Assets/Script/CheckPoint.cs
@@ -6,20 +6,22 @@ public class CheckPoint : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float jumpForce = 20f;
-    public GameObject CP, player;
+    public GameObject checkP, player;
 
     private bool isActivated = false;
     private Vector3 checkPos, playerPos;
-    private bool isActive = true;
     private bool cpActived = false;
-
-
+    private bool isColorChanged = false;
+    private Renderer rend;
+    private Color col;
+    private bool isColliderCheckP = false, isCollidedDeath = false;
 
     // Use this for initialization
     void Start()
     {
-        checkPos = CP.transform.position;
+        checkPos = checkP.transform.position;
         playerPos = player.transform.position;
+        rend = checkP.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -54,27 +56,49 @@ public class CheckPoint : MonoBehaviour
     }
     public IEnumerator Wait()
     {
-        cpActived = true;
-        player.SetActive(false);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5);
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.collider.tag == "checkpoint")
+        if(other.gameObject.tag == "checkpoint")
         {
-            StartCoroutine(Wait());
-            player.transform.position = Vector3.zero;
-            player.SetActive(true);
+            rend.material.color = Color.cyan;
+            isColliderCheckP = true;
+        }
+        else if(other.gameObject.tag == "death")
+        {
+            isCollidedDeath = true;
+        }
+        if(isCollidedDeath && isColliderCheckP)
+        {
+            isColliderCheckP = false;
+            isCollidedDeath = false;
+            ActivateCheckPoint();
+            DieAndSpawn();
         }
 
+        //NO DEJA METER UN IF DENTRO DE OTRO IF Y QUE EL SEGUNDO IF MIRE UNA COLISION
 
     }
-
+    /*void OnCollisionExit(Collision coll)
+    {
+        if (coll.gameObject.tag == "death")
+            isCollidedDeath = false;
+        else if (coll.gameObject.tag == "checkpoint")
+            isColliderCheckP = false;
+    }*/
 
     void ActivateCheckPoint()
     {
+        cpActived = true;
+        isColorChanged = true;
+    }
 
+    void DieAndSpawn()
+    {
+        player.SetActive(true);
+        player.transform.position = checkP.transform.position;
     }
 }
-   
+
