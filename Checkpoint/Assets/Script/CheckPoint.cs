e247f4be8feff9407984a9ed9577c3056765fe7b
Marc Alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-03-17 15:52:40 +0100

    en principio ahora si estaria termnado

--- Checkpoint/Assets/Script/CheckPoint.cs
+++ Checkpoint/Assets/Script/CheckPoint.cs
@@ -4,54 +4,54 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed = 20f;
     public float jumpForce = 20f;
-    public GameObject checkP, player;
+    public GameObject checkPoint;
 
-    private bool isActivated = false;
+    //private bool isActivated = false;
     private Vector3 checkPos, playerPos;
-    private bool cpActived = false;
-    private bool isColorChanged = false;
     private Renderer rend;
     private Color col;
-    private bool isColliderCheckP = false, isCollidedDeath = false;
+    private bool isCollidedDeath = false;
+    private bool isCollidedCheckP = false;
+    private bool cpActivated = false;
+    private bool isColorChanged = false;
 
     // Use this for initialization
     void Start()
     {
-        checkPos = checkP.transform.position;
-        playerPos = player.transform.position;
-        rend = checkP.GetComponent<Renderer>();
+        checkPos = checkPoint.transform.position;
+        playerPos = transform.position;
+        rend = checkPoint.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
-        //Debug.Log(playerPos);
     }
 
     void Movement()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            player.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            player.transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            player.transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            player.transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
         }
         else if (Input.GetKey(KeyCode.Space))
         {
-            player.transform.Translate(Vector3.up * Time.deltaTime * jumpForce);
+            transform.Translate(Vector3.up * Time.deltaTime * jumpForce);
         }
     }
     public IEnumerator Wait()
@@ -61,44 +61,34 @@ public class CheckPoint : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "checkpoint")
+        if (other.collider.tag == "checkpoint")
         {
             rend.material.color = Color.cyan;
-            isColliderCheckP = true;
+            isCollidedCheckP = true;
         }
-        else if(other.gameObject.tag == "death")
+        if (other.collider.tag == "death")
         {
             isCollidedDeath = true;
         }
-        if(isCollidedDeath && isColliderCheckP)
+        if (isCollidedDeath && isCollidedCheckP)
         {
-            isColliderCheckP = false;
+            isCollidedCheckP = false;
             isCollidedDeath = false;
             ActivateCheckPoint();
             DieAndSpawn();
         }
 
-        //NO DEJA METER UN IF DENTRO DE OTRO IF Y QUE EL SEGUNDO IF MIRE UNA COLISION
-
     }
-    /*void OnCollisionExit(Collision coll)
-    {
-        if (coll.gameObject.tag == "death")
-            isCollidedDeath = false;
-        else if (coll.gameObject.tag == "checkpoint")
-            isColliderCheckP = false;
-    }*/
+
 
     void ActivateCheckPoint()
     {
-        cpActived = true;
+        cpActivated = true;
         isColorChanged = true;
     }
 
     void DieAndSpawn()
     {
-        player.SetActive(true);
-        player.transform.position = checkP.transform.position;
+        transform.position = checkPoint.transform.position;
     }
 }
-
---
+++ Checkpoint/Assets/Script/Collisions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+
+    private bool isCollidedDeath = false;
+    [HideInInspector]public bool isCollidedCheckP = false;
+    private bool cpActivated = false;
+    private bool isColorChanged = false;
+    public GameObject checkP;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    
+}
