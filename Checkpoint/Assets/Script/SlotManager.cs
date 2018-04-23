3249f9592607b9d72572d5222b57c26ed01dcb1d
ALFONSO_SECANELLA <marc.alfonso@balmes.escolapia.net>
2018-04-23 17:58:15 +0200

    Ya va teniendo forma

--- Checkpoint/Assets/Script/SlotManager.cs
+++ Checkpoint/Assets/Script/SlotManager.cs
@@ -8,6 +8,9 @@ public class SlotManager : MonoBehaviour
 
 
     public Button load_1, load_2, load_3, save_1, save_2, save_3;
+    public Player pa;
+    private Vector3 player_position;
+
     void Start()
     {
         save_1 = GetComponent<Button>();
@@ -18,37 +21,123 @@ public class SlotManager : MonoBehaviour
         load_3 = GetComponent<Button>();
 
     }
+
+    /*********************************************************************************************************************************************************************************************************************/
+    /*********************************************************************************************************************************************************************************************************************/
     public void Save_Slot_One()
     {
+        player_position = pa.transform.position;
 
+        if (!Directory.Exists("Slot_1"))
+        {
+            Directory.CreateDirectory("Slot_1");
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Create("save_one.sav");
+
+        Position pos = new Position();
+        pos.pos.x = player_position.x;
+        pos.pos.y = player_position.y;
+        pos.pos.z = player_position.z;
+
+        formatter.Serialize(file, pos);
+        file.Close();
     }
 
     public void Save_Slot_Two()
     {
+        player_position = pa.transform.position;
 
+        if (!Directory.Exists("Slot_2"))
+        {
+            Directory.CreateDirectory("Slot_2");
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Create("save_two.sav");
+
+        Position pos = new Position();
+        pos.pos.x = player_position.x;
+        pos.pos.y = player_position.y;
+        pos.pos.z = player_position.z;
+
+        formatter.Serialize(file, pos);
+        file.Close();
     }
+
 
     public void Save_Slot_Three()
     {
+        player_position = pa.transform.position;
 
+        if (!Directory.Exists("Slot_3"))
+        {
+            Directory.CreateDirectory("Slot_3");
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Create("save_three.sav");
+
+        Position pos = new Position();
+        pos.pos.x = player_position.x;
+        pos.pos.y = player_position.y;
+        pos.pos.z = player_position.z;
+
+        formatter.Serialize(file, pos);
+        file.Close();
     }
+
+    /********************************************************************************************************************************************************************************************************************/
+    /********************************************************************************************************************************************************************************************************************/
 
     public void Load_Slot_One()
     {
-       
+
+        if (File.Exists("save_one.sav"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead("save_one.sav");
+
+            Position data = (Position)bf.Deserialize(file);
+
+            pa.transform.position = new Vector3(data.pos.x, data.pos.y, data.pos.z);
+            file.Close();
+        }
     }
 
     public void Load_Slot_Two()
     {
+        if (File.Exists("save_two.sav"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead("save_two.sav");
 
+            Position data = (Position)bf.Deserialize(file);
+
+            pa.transform.position = new Vector3(data.pos.x, data.pos.y, data.pos.z);
+            file.Close();
+        }
     }
 
     public void Load_Slot_Three()
     {
+        if (File.Exists("save_three.sav"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead("save_three.sav");
 
+            Position data = (Position)bf.Deserialize(file);
+
+            pa.transform.position = new Vector3(data.pos.x, data.pos.y, data.pos.z);
+            file.Close();
+        }
     }
 
+    /********************************************************************************************************************************************************************************************************************/
+    /********************************************************************************************************************************************************************************************************************/
 
+    [Serializable]
     class Position
     {
         [Serializable]
@@ -63,5 +152,6 @@ public class SlotManager : MonoBehaviour
                 z = _z;
             }
         }
+        public Vector3 pos = new Vector3(0, 0, 0);
     }
 }
