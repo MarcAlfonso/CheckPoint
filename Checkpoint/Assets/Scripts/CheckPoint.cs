246c827342e720789737967590b8bde8abc06c72
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-05-03 17:57:42 +0200

    Nueva escena de loading screen

---
+++ Checkpoint/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    //Public variables
+    public GameObject checkPoint, ObjToRend;
+    public bool isCollidedCheckP = false;
+
+    //Private variables
+    private Vector3 checkPos, playerPos;
+    private Renderer rend, rend2;
+    private Color col;
+    private bool isCollidedDeath = false;
+    private bool cpActivated = false;
+    private bool isColorChanged = false;
+    private GameData gd;
+    
+
+
+    void Start()
+    {
+        checkPos = checkPoint.transform.position;
+        playerPos = transform.position;
+        rend = checkPoint.GetComponent<Renderer>();
+        rend2 = ObjToRend.GetComponent<Renderer>();
+        gd = FindObjectOfType<GameData>();
+    }
+    
+    //Function to detect the collisions between the Player and the Checkpoint / Death
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "checkpoint")
+        {
+            ChangeColor();
+            gd.SaveGame();            
+            isCollidedCheckP = true;
+        }
+        if (col.tag == "death")
+        {
+            isCollidedDeath = true;
+            transform.position = Vector3.zero;
+        }
+        if (isCollidedDeath && isCollidedCheckP)
+        {
+            isCollidedCheckP = false;
+            isCollidedDeath = false;
+            ActivateCheckPoint();
+            DieAndSpawn();
+        }
+
+    }
+
+    //Function to activate the checkpoint
+    void ActivateCheckPoint()
+    {
+        cpActivated = true;
+        isColorChanged = true;
+    }
+
+    void DieAndSpawn()
+    {
+        transform.position = checkPoint.transform.position;
+    }
+
+    public void ChangeColor()
+    {
+        rend.material.color = Color.cyan;
+        rend2.material.color = Color.cyan;
+    }
+}
---
+++ Checkpoint/Assets/Scripts/GameData.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+
+    //PUBLIC VARIABLES
+    public GameObject player;
+    public Button load;
+
+
+    //PRIVATE VARIABLES
+    private string path = "save.sav";
+    private Player pa;
+    private Vector3 playerPosition;
+    private bool isEnemyAlive = true;
+
+    //INICIALIZAMOS EL LOAD AL COMPONENTE BOTON
+    void Start()
+    {
+        load = GetComponent<Button>();
+    }
+
+    //AL PRESIONAR EL BOTON LOAD LLAMAMOS A LA FUNCION QUE CARGA EL FICHERO CON LA NUEVA POSICION
+    public void OnClickLoad()
+    {
+        LoadGame();
+    }
+
+
+    //FUNCIÓN PARA GUARDAR LA POSICIÓN DEL PLAYER
+    public void SaveGame()
+    {
+        playerPosition = player.transform.position;
+        //isEnemyAlive = pa.isAlive;
+        //SI LA CARPETA NO EXISTE LA CREAMOS
+        if (!Directory.Exists("SaveGame"))
+        {
+            Directory.CreateDirectory("SaveGame");
+        }
+
+        //CREAMOS EL FICHERO QUE CONTENDRA LOS DATOS
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Create(path); //PATH = "SAVE.SAV"
+         
+        Data data = new Data();
+        //GUARDAMOS LOS DATOS DEL PLAYER
+        data.pos.x = playerPosition.x;
+        data.pos.y = playerPosition.y;
+        data.pos.z = playerPosition.z;
+        /*data.isAlive = isEnemyAlive;*/
+
+        formatter.Serialize(file, data);
+        file.Close();
+
+    }
+
+    //FUNCIÓN PARA CARGAR LA POSICION DEL PLAYER QUE PREVIAMENTE HEMOS GUARDADO EN 3 VARIABLES (DATA.POS)
+    public void LoadGame()
+    {
+        //SI EL FICHERO EXISTE LO ABRIMOS Y LO LEEMOS
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead(path);
+
+            Data data = (Data)bf.Deserialize(file);
+
+            //CAMBIAMOS LA POSICION DEL PLAYER POR LA POSICION GUARDADA
+            player.transform.position = new Vector3(data.pos.x, data.pos.y, data.pos.z);
+            file.Close();
+        }
+    }
+
+
+    //SERIALIZAMOS LA CLASE PARA PODER GUARDAR LOS DATOS
+    [Serializable]
+    class Data
+    {
+        [Serializable]
+        public struct Vector3 {
+            public float x, y, z;
+
+            public Vector3(float _x, float _y, float _z)
+            {
+                x = _x;
+                y = _y;
+                z = _z;
+            }
+        }
+        public Vector3 pos = new Vector3(1,2,1);
+        public int score = 50;
+        public bool isAlive = true;
+    }
+}
