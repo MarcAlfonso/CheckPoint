0252e87843955efab83a2b0eecc5fe5e02818931
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-05-07 17:48:28 +0200

    starting the loading screen

---
+++ Checkpoint/Assets/Script/LoadingScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingScreen : MonoBehaviour {
+
+    AsyncOperation async;
+    public int mainScene;
+    private bool changeScene = true;
+
+    public void ChangeScene()
+    {
+        if (changeScene)
+        {
+            StartCoroutine(LoadingTime());
+            SceneManager.LoadScene(mainScene);
+        }else
+        {
+            Debug.Log("error while opening the new scene!");
+        }
+    }
+
+
+    IEnumerator LoadingTime()
+    {
+        yield return new WaitForSeconds(3f);
+
+        async = SceneManager.LoadSceneAsync(mainScene);
+        async.allowSceneActivation = false;
+
+        while(!async.isDone)
+        {
+            yield return new WaitForSeconds(2f);
+            if (async.isDone)
+            {
+                async.allowSceneActivation = true;
+                SceneManager.LoadScene(mainScene);
+            }
+            Debug.Log(async.progress);
+            yield return null;
+        }
+
+    }
+}
