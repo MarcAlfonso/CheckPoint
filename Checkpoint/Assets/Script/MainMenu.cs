90cfffc01a521969270b253400f15f76bc985883
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-05-08 20:20:10 +0200

    MainMenu, newgame button done

---
+++ Checkpoint/Assets/Script/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button continueButton, newGameButton, optionsButton;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickNewGameButton()
+    {
+        StartCoroutine(SceneChange());
+    }
+
+    IEnumerator SceneChange()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("GameScene");
+    }
+}
